685037566cf21cbf5d4d3a175f4afd00c3d6cff1
Sergey Savrukhin <savra.sv@yandex.ru>
2022-10-31 21:15:09 +0300

    validation

--- src/main/java/com/hvdbs/springcourse/controller/PeopleController.java
+++ src/main/java/com/hvdbs/springcourse/controller/PeopleController.java
@@ -2,10 +2,12 @@ package com.hvdbs.springcourse.controller;
 
 import com.hvdbs.springcourse.dao.PersonDAO;
 import com.hvdbs.springcourse.model.Person;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -38,8 +40,42 @@ public class PeopleController {
     }
 
     @PostMapping
-    public String create(@ModelAttribute Person person) {
+    public String create(@ModelAttribute @Valid Person person,
+                         BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "people/new";
+        }
+
         personDAO.save(person);
+
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable int id) {
+        model.addAttribute("person", personDAO.show(id));
+
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
+
+        personDAO.update(id, person);
+
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable int id) {
+        personDAO.delete(id);
 
         return "redirect:/people";
     }
--- src/main/java/com/hvdbs/springcourse/dao/PersonDAO.java
+++ src/main/java/com/hvdbs/springcourse/dao/PersonDAO.java
@@ -14,10 +14,10 @@ public class PersonDAO {
     {
         people = new ArrayList<>();
 
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT, "Kate"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 24, "tom@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 52, "bob@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 18, "mike@yahoo.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Kate", 34, "kate@gmail.com"));
     }
 
     public List<Person> index() {
@@ -35,5 +35,17 @@ public class PersonDAO {
         person.setId(++PEOPLE_COUNT);
 
         people.add(person);
+    }
+
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
+    }
+
+    public void delete(int id) {
+        people.removeIf(person -> person.getId() == id);
     }
 }
